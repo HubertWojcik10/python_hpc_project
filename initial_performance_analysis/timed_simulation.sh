73ea4c40d2ae3ac80c40fee4f2fef0d61acf9051
HubertWojcik10 <hubert.wojcik002@gmail.com>
2025-03-25 15:40:59 +0100

    task 1&2

--- initial_performance_analysis/timed_simulation.sh
+++ initial_performance_analysis/timed_simulation.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-#BSUB -J timed_simulation_task2
+#BSUB -J timed_simulation_task2_5N
 #BSUB -q hpc
-#BSUB -W 2
-#BSUB -R "rusage[mem=1024]"
+#BSUB -W 20
+#BSUB -R "rusage[mem=2048]"
 #BSUB -R "select[model==XeonGold6126]"
-#BSUB -o timed_simulation_task2_%J.out
-#BSUB -e timed_simulation_task2_%J.err
-#BSUB -n 4
+#BSUB -o timed_simulation_task2_5N_%J.out
+#BSUB -e timed_simulation_task2_5N_%J.err
+#BSUB -n 8
 #BSUB -R "span[hosts=1]"
 
 # use lsinfo -m to get the CPU model name
-python timed_simulation.py 20
+python timed_simulation.py 5
