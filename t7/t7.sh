dbff014ee7809d24e84cca13245dfe47433e84fa
HubertWojcik10 <hubert.wojcik002@gmail.com>
2025-04-12 19:14:30 +0200

    t7

--- t7/t7.sh
+++ t7/t7.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
-#BSUB -J t7_100N
+#BSUB -J t7_1000N
 #BSUB -q hpc
-#BSUB -W 20
-#BSUB -R "rusage[mem=2048]"
+#BSUB -W 120
+#BSUB -R "rusage[mem=4096]"
 #BSUB -R "select[model==XeonGold6126]"
-#BSUB -o t7_100N_5N_%J.out
-#BSUB -e t7_100N_%J.err
+#BSUB -o t7_1000N_%J.out
+#BSUB -e t7_1000N_%J.err
 #BSUB -n 8
 #BSUB -R "span[hosts=1]"
+
+source /dtu/projects/02613_2025/conda/conda_init.sh
+conda activate 02613
 
 python simulate.py 1000
