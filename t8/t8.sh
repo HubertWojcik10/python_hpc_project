80759056e9bad1b4b4f5a760b3baba22f6aad383
HubertWojcik10 <hubert.wojcik002@gmail.com>
2025-04-21 13:27:51 +0200

    task 8 done

--- t8/t8.sh
+++ t8/t8.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 #BSUB -q c02613
-#BSUB -J t8_100N
+#BSUB -J t8_1000N
 #BSUB -n 4
 #BSUB -R "span[hosts=1]"
 #BSUB -R "rusage[mem=1GB]"
 #BSUB -gpu "num=1:mode=exclusive_process"
 #BSUB -W 00:30
-#BSUB -o t8_100N_%J.out
-#BSUB -e t8_100N_%J.err
+#BSUB -o t8_1000N_%J.out
+#BSUB -e t8_1000N_%J.err
 
 source /dtu/projects/02613_2025/conda/conda_init.sh
 conda activate 02613
 
-time python simulate.py 100
+python simulate.py 1000
